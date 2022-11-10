b4cf6a8b911791b3a8534797dace6d529b797300
jmbruno93 <117466294+jmbruno93@users.noreply.github.com>
2022-11-10 14:06:29 +0100

    Add files via upload

---
+++ AssetsManager.java
@@ -0,0 +1,9 @@
+package com.politecnicomalaga.sp2.managers;
+
+public class AssetsManager {
+    //Constantes
+    public static final String NUMBERS_SPRITES = "numbers/digito";
+    public static final String NUMBERS_EXT_SPRITES =".png";
+
+    public static final String ATLAS_FILE = "sp2.atlas";
+}
---
+++ SettingsManager.java
@@ -0,0 +1,21 @@
+package com.politecnicomalaga.sp2.managers;
+
+public class SettingsManager {
+
+    //Constantes
+    public static final short SCREEN_WIDTH = 1024;
+    public static final short SCREEN_HEIGHT = 768;
+
+    //escuadrón(columnas) = 8 y batallón(filas) = 4
+    //usar la mitad del tamaño en los batallones y el jugador para las colisiones
+    public static final short ENEMIES_PER_SQUADRON = 8;
+    public static final short SQUADRON_PER_BATALLON = 4;
+    public static final short ENEMIES_SIZE = 64;
+    public static final short PLAYER_SIZE = 64;
+    public static final short MIDENEMIES_SIZE = 32;
+    public static final short MIDPLAYER_SIZE = 32;
+
+    //public static final short ENEMY_ANIMATION_VEL = ;
+    //public static final short PLAYER_ANIMATION_VEL = ;
+
+}
