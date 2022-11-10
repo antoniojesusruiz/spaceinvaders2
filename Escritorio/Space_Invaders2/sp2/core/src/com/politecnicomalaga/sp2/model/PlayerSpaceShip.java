72556a5ed7c9fd7240953565f8b8b844cd0decee
aru-ms <104093401+aru-ms@users.noreply.github.com>
2022-11-10 20:55:35 +0100

    Add files via upload
    Subo la clase PlayerSpaceShip

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.politecnicomalaga.sp2.managers.GameManager;
+import com.politecnicomalaga.sp2.managers.SettingsManager;
+
+import java.util.Set;
 
 public class PlayerSpaceShip extends Actor {
 
@@ -14,14 +17,20 @@ public class PlayerSpaceShip extends Actor {
 
     public PlayerSpaceShip() {
         super();
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("images/player.pack"));
-        skin = new Animation<TextureRegion>(0.25f, atlas.findRegions("blue"), Animation.PlayMode.LOOP);
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(SettingsManager.ATLAS_PATH));
+        skin = new Animation<TextureRegion>(SettingsManager.ANIMATION_SPEED, atlas.findRegions(SettingsManager.PLAYER_PATH), Animation.PlayMode.LOOP);
 
     }
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         TextureRegion currentFrame = skin.getKeyFrame(GameManager.getSingleton().getGameTime(), true);
-        batch.draw(currentFrame, this.getX(), this.getY());
+        batch.draw(currentFrame, SettingsManager.SCREEN_WIDTH/2, SettingsManager.SHIP_HEIGHT/2);
+    }
+
+    public void dispose() {
+        if (this!=null) {
+            this.dispose();
+        }
     }
 }
