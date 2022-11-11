719be9ed9b2b5e4d2f50ed242f8b68f6ac9769f0
Oeaon <65346324+Oeaon@users.noreply.github.com>
2022-11-11 13:43:14 +0100

    Add files via upload

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/Battalion.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/Battalion.java
@@ -1,21 +1,85 @@
 package com.politecnicomalaga.sp2.model;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 
 import java.util.ArrayList;
 
 public class Battalion {
-    private Array<Squadron> squadrons;
+
+    //DECLARACIONES
+    private Array<Squadron> batallon;
+
+
+     //(CONSTANTES DEL GUILLE)
+
+   protected static int CONSTANTEnumESCUADRONES;
+    protected static int CONSTANTEaltoEscuadron=0;
+    protected static int CONSTANTEanchoEscuadron=0;
+
+
+
+
 
     public Battalion(Stage baseStage) {
         //Initiate the arraylist
-        squadrons = new Array<Squadron>();
+        batallon = new Array<Squadron>();
 
 
         //We have to create all the squadrons
         for (int i=0;i<4;i++) {
             Squadron newSquad = new Squadron(baseStage);
         }
+    }
+
+    //CONSTRUCTORES
+
+    public void Batallon(int ancho, int alto){
+        ancho= CONSTANTEanchoEscuadron;
+        alto= CONSTANTEaltoEscuadron;
+
+        float distanciaEntreEscuadrones = (alto/2)/CONSTANTEnumESCUADRONES;
+
+        //lista de escuadrones
+        batallon= new Array<>();
+
+        //Crear escuadrones
+
+        for (int i=0; i<CONSTANTEnumESCUADRONES;i++){
+            Squadron escuadron = new Squadron(ancho,alto-distanciaEntreEscuadrones*(i+1));
+            batallon.add(escuadron);
+        }
+    }
+
+    public void pintarse (SpriteBatch sb){
+        for (Squadron sq : batallon){
+            sq.pintarse(sb);
+        }
+    }
+
+    public void dispose(){
+        for (Squadron sq : batallon){
+            sq.dispose();
+        }
+    }
+
+    //Saber nº de naves que han sobrevivido
+    public int NavesRestantes(){
+        int restantes=0;
+        for (Squadron sq : batallon){
+            restantes+=sq.numNaves();
+        }
+        return restantes;
+    }
+
+    public boolean destruido(){
+        boolean estado=false;
+
+        if (NavesRestantes()==0){
+            estado=true;
+        }
+
+        return estado;
     }
 }
--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/Squadron.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/Squadron.java
@@ -1,21 +1,103 @@
 package com.politecnicomalaga.sp2.model;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 
 public class Squadron {
 
-    private Array<EnemyShip> troops;
+    //ATRIBUTOS
+
+    protected Array<EnemyShip> tropas;
+    protected boolean right;
+
+    //(CONSTANTES DEL GUILLE)
+    int CONSTANTEnumNAVES;
+
+
 
     public Squadron(Stage baseStage) {
         //Initiate the arraylist
-        troops = new Array<EnemyShip>();
+
+        tropas = new Array<EnemyShip>();
 
 
         //We have to create all the squadrons
-        for (int i=0;i<4;i++) {
+
+        for (int i=0;i<8;i++) {
             EnemyShip newEnemy = new EnemyShip();
             baseStage.addActor(newEnemy);
         }
     }
+
+    //CONSTRUCTORES
+
+    public Squadron(int widthScreen, float vertPosition){
+        EnemyShip alien;
+        float distanceBTWships= widthScreen/(CONSTANTEnumNAVES+1);
+        right=true;
+        for (int i=0;i<CONSTANTEnumNAVES;i++){
+            alien = new EnemyShip();
+            alien.setBounds((i+1)*distanceBTWships,vertPosition,"AnchoGuille","altoGuille");
+            tropas.add(alien);
+        }
+    }
+
+    //PINTARSE
+
+    public void pintarse (SpriteBatch sb){
+        for (EnemyShip et : tropas){
+---------------------//ESPERAR A QUE HAGAN EL MÉTODO PARA PINTARSE--------------------
+            et.pintarse(sb);
+        }
+    }
+
+    //MOVERSE
+    public void moverse(){
+        for (EnemyShip et : tropas){
+---------------------//ESPERAR A QUE HAGAN EL MÉTODO PARA MOVERSE--------------------
+            et.moverse();
+        }
+    }
+
+    //DISPOSE
+
+    public void dispose(){
+        for (EnemyShip et : tropas){
+            ---------------------//ESPERAR A QUE HAGAN EL MÉTODO DISPOSE--------------
+            et.dispose();
+        }
+    }
+
+    //numNaves
+
+    public int numNaves(){
+        return tropas.size;
+    }
+
+    //COLISIÓN
+
+    //(HACE FALTA LA CLASE "objetoVolador"
+    public boolean colision (????){
+
+    }
+
+    //DISPARAR
+
+    //HACE FALTA LA CLASE "DisparoEnemigo"
+    public DisparoAlien disparo(){
+        DisparoEnemigo tirito = null;
+        EnemyShip naveDisparando;
+        int pos;
+
+        if (!tropas.isEmpty()){
+            pos = (int) (Math.random()*(tropas.size-1));
+
+            naveDisparando= tropas.get(pos);
+
+            tirito = new
+        }
+    }
+
+
 }
