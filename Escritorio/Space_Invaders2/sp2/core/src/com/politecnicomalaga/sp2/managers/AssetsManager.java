e42b117532056f3800df1b29f7b15c0dce088a57
aru-ms <104093401+aru-ms@users.noreply.github.com>
2022-11-11 13:46:27 +0100

    Update AssetsManager.java

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/managers/AssetsManager.java
@@ -5,4 +5,5 @@ public class AssetsManager {
     public static final String NUMBERS_SPRITES = "numbers/digito";
     public static final String NUMBERS_EXT_SPRITES =".png";
 
+    public static final String ATLAS_FILE = "sp2.atlas";
 }
