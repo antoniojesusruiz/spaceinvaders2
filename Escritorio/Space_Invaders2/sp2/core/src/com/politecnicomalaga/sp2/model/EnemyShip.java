6d41824fc2b13e45294aa3f6ecf1998c76bbf27c
AntonioRuiz <92110214+antoniojesusruiz@users.noreply.github.com>
2022-11-11 13:40:39 +0100

    Add files via upload

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/EnemyShip.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/EnemyShip.java
@@ -12,17 +12,31 @@ import com.politecnicomalaga.sp2.managers.GameManager;
 public class EnemyShip extends Actor {
 
     private Animation<TextureRegion> skin;
+    //ESTE STRING ES PROVISIONAL, CUANDO TENGAMOS EL ASSETMANAGER LISTO,TENEMOS QUE CAMBIARLO
+    private String ATLAS;
 
     public EnemyShip() {
         super();
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("images/enemy.pack"));
-        skin = new Animation<TextureRegion>(0.25f, atlas.findRegions("green_enemy"), Animation.PlayMode.LOOP);
+        //DIFERENTES OPCIONES PARA VER CUAL FUNCIONA
+
+        //TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("images/enemy.pack"));
+        //TextureAtlas atlas = new TextureAtlas(ATLAS);
+        //TextureAtlas atlas= new TextureAtlas(Gdx.files.internal("sp2.png"));
+        TextureAtlas atlas= new TextureAtlas(Gdx.files.internal("sp2.atlas"));
+        skin = new Animation<TextureRegion>(0.25f, atlas.findRegions("ovni"), Animation.PlayMode.LOOP);
 
     }
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         TextureRegion currentFrame = skin.getKeyFrame(GameManager.getSingleton().getGameTime(), true);
-        batch.draw(currentFrame, this.getX(), this.getY());
+        //batch.draw(currentFrame, this.getX(), this.getY());
+        //LA ANCHURA Y EL ALTO HAY QUE PONERLO CON LAS CONSTANTES QUE TIENE QUE HACER GUILLE
+        batch.draw(currentFrame,this.getX(),this.getY(),this.getWidth(),this.getHeight());
+    }
+    public void dispose(){
+        if(this!=null){
+            this.dispose();
+        }
     }
 }
