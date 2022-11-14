f98af91621b3b0f7d9ef4104309f0955803869b7
AntonioRuiz <92110214+antoniojesusruiz@users.noreply.github.com>
2022-11-14 14:51:59 +0100

    main Actualizado

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/GdxSpaceInvaders2.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/GdxSpaceInvaders2.java
@@ -6,17 +6,21 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.politecnicomalaga.sp2.managers.GameManager;
 import com.politecnicomalaga.sp2.managers.SettingsManager;
+import com.politecnicomalaga.sp2.view.GameScreen;
 
 public class GdxSpaceInvaders2 extends Game {
+	GameScreen screen;
 	SpriteBatch batch;
     OrthographicCamera camera;
 
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 
+		batch = new SpriteBatch();
+		screen= new GameScreen(this);
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, SettingsManager.SCREEN_WIDTH, SettingsManager.SCREEN_HEIGHT);
 	}
@@ -25,7 +29,7 @@ public class GdxSpaceInvaders2 extends Game {
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-
+		screen.render(GameManager.getSingleton().getGameTime());
 		batch.end();
 	}
 	
