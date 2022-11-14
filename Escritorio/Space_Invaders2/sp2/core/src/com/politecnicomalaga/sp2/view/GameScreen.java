4897291af00545d32caabf752782667f5daba053
amormun062 <117466168+amormun062@users.noreply.github.com>
2022-11-14 16:39:38 +0100

    Update GameScreen.java

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/view/GameScreen.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/view/GameScreen.java
@@ -27,9 +27,6 @@ public class GameScreen implements Screen {
     public GameScreen(Game aGame) {
         game = aGame;
 
-	//Esta orden se puede poner también en el show()
-        Gdx.input.setInputProcessor(stage);
-
         stage = new Stage(new ScreenViewport());
 
         //We add the battalion, "the empire"
@@ -72,14 +69,12 @@ public class GameScreen implements Screen {
     @Override
     public void show() {
         Gdx.app.log("MainScreen","show");
-
+	Gdx.input.setInputProcessor(stage);
     }
 
     //Renderiza el escenario
     @Override
     public void render(float delta) {
-        //jave 8
-
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(delta);
