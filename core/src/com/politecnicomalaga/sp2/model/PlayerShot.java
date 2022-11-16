2c7aa8f4836fc50c73f46accaac58227c33793a4
Alberto Martín <vhiek.ams@gmail.com>
2022-11-16 20:59:51 +0100

    Termino el draw y empiezo el act

--- core/src/com/politecnicomalaga/sp2/model/PlayerShot.java
+++ core/src/com/politecnicomalaga/sp2/model/PlayerShot.java
@@ -1,8 +1,12 @@
 package com.politecnicomalaga.sp2.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.politecnicomalaga.sp2.managers.AssetsManager;
 import com.politecnicomalaga.sp2.managers.SettingsManager;
 
 public class PlayerShot extends Actor {
@@ -14,6 +18,20 @@ public class PlayerShot extends Actor {
         super();
         setBounds(0,0, SettingsManager.SHOT_WIDTH, SettingsManager.SHOT_HEIGHT);
         setX(shotOwner.getX());
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(AssetsManager.ATLAS_FILE));
         setY(shotOwner.getY());
+        skin = new TextureRegion(atlas.findRegion("disparo_aliado")).getTexture();
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        batch.draw(skin, this.getX(), this.getY());
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
     }
 }
