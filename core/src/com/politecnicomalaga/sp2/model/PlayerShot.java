dfd3649e01fa6b5a8ceea04e2eac8256ada29e91
Alberto Martín <vhiek.ams@gmail.com>
2022-11-16 19:57:30 +0100

    Avanzar ligeramente con el disparo

--- core/src/com/politecnicomalaga/sp2/model/PlayerShot.java
+++ core/src/com/politecnicomalaga/sp2/model/PlayerShot.java
@@ -1,4 +1,19 @@
 package com.politecnicomalaga.sp2.model;
 
-public class PlayerShot {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.politecnicomalaga.sp2.managers.SettingsManager;
+
+public class PlayerShot extends Actor {
+
+    private Texture skin;
+    private PlayerSpaceShip shotOwner;
+
+    public PlayerShot() {
+        super();
+        setBounds(0,0, SettingsManager.SHOT_WIDTH, SettingsManager.SHOT_HEIGHT);
+        setX(shotOwner.getX());
+        setY(shotOwner.getY());
+    }
 }
