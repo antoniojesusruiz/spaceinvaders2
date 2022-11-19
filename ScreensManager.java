388bc60534f3afb6f19f808e89a7a35eb35fb49d
jmbruno93 <117466294+jmbruno93@users.noreply.github.com>
2022-11-19 19:47:02 +0100

    Update ScreensManager.java

--- ScreensManager.java
+++ ScreensManager.java
@@ -2,38 +2,49 @@ package com.politecnicomalaga.sp2.managers;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.politecnicomalaga.sp2.view.ConfigScreen;
+import com.politecnicomalaga.sp2.view.CreditsScreen;
+import com.politecnicomalaga.sp2.view.GameOverScreen;
 import com.politecnicomalaga.sp2.view.GameScreen;
+import com.politecnicomalaga.sp2.view.SplashScreen;
 
 public class ScreensManager
 {
     private static ScreensManager singleton;
 
-    private enum SCREENS {
-        GAME_SCREEN, GAMEOVER_SCREEN, SPLASH_SCREEN, CREDITS_SCREEN
+    public enum SCREENS {
+        GAME_SCREEN, GAMEOVER_SCREEN, SPLASH_SCREEN, CREDITS_SCREEN, SETTINGS_SCREEN
     }
 
     private ScreensManager(){
 
     }
 
-    public void getScreen(Game aGame, SCREENS screenToGet)
+    public Screen getScreen(Game aGame, SCREENS screenToGet)
     {
         Screen newScreen;
         switch(screenToGet)
         {
             //Create Enum: GAME_SCREEN GAMEOVER_SCREEN, SPLASH_SCREEN, CREDITS_SCREEN
             case GAME_SCREEN: newScreen = new GameScreen(aGame);
-            break;
+            return newScreen;
 
             case GAMEOVER_SCREEN: newScreen = new GameOverScreen(aGame);
-            break;
+                return newScreen;
+
 
             case SPLASH_SCREEN: newScreen = new SplashScreen(aGame);
-            break;
+                return newScreen;
+
 
             case CREDITS_SCREEN: newScreen = new CreditsScreen(aGame);
-            break;
+                return newScreen;
+
+            case SETTINGS_SCREEN: newScreen = new ConfigScreen(aGame);
+                return newScreen;
+
         }
+        return null;
     }
 
     public static ScreensManager getSingleton()
@@ -46,3 +57,4 @@ public class ScreensManager
     }
 
 }
+ 
