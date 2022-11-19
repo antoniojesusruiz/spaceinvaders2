fca268551439ea437b179ae2d6e254d6d720e700
AntonioRuiz <92110214+antoniojesusruiz@users.noreply.github.com>
2022-11-19 13:40:00 +0100

    Jugador que dispara y PlayerShot con textura

---
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/PlayerShot.java
@@ -0,0 +1,46 @@
+package com.politecnicomalaga.sp2.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.politecnicomalaga.sp2.managers.*;
+
+
+public class PlayerShot extends Actor {
+
+    private Texture skin;
+
+    public PlayerShot(PlayerSpaceShip shotOwner) {
+        super();
+        setBounds(shotOwner.getX(),shotOwner.getY(), SettingsManager.SHOT_WIDTH, SettingsManager.SHOT_HEIGHT);
+        // Toma las posiciones de la nave principal y un poco de altura para no aparecer en el
+        // mismo sitio.
+        setX(shotOwner.getX());
+        setY(shotOwner.getY()+SettingsManager.PLAYER_SIZE);
+       // TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(AssetsManager.ATLAS_FILE));
+        //skin = new TextureRegion(atlas.findRegion("disparo_aliado")).getTexture();
+         skin= new Texture(Gdx.files.internal("images/disparo_aliado.png"));
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        batch.draw(skin, this.getX(), this.getY());
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        // Se mueve verticalmente
+        this.setY(this.getY()+1);
+    }
+
+    public void dispose() {
+        if (this!=null) {
+            this.dispose();
+        }
+    }
+}
--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/model/PlayerSpaceShip.java
@@ -6,24 +6,32 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.politecnicomalaga.sp2.managers.AssetsManager;
 import com.politecnicomalaga.sp2.managers.GameManager;
 import com.politecnicomalaga.sp2.managers.SettingsManager;
+import com.politecnicomalaga.sp2.view.GameScreen;
 
 import java.util.Set;
 
 public class PlayerSpaceShip extends Actor {
 
     private Animation<TextureRegion> skin;
+    private Array<PlayerShot> disparosActivos =new Array<PlayerShot>(); //disparos que estan dentro de la pantalla
+    private Array<PlayerShot> recamara =new Array<PlayerShot>();; //disparos que estan fuera de la pantalla
+    private float timeToFire;
+    private Stage stage;
 
-    public PlayerSpaceShip() {
+    public PlayerSpaceShip(Stage stage) {
         super();
+        timeToFire=0.0f;
         setBounds(0,0, SettingsManager.PLAYER_SIZE, SettingsManager.PLAYER_SIZE);
         TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(AssetsManager.ATLAS_FILE));
         setX(SettingsManager.SCREEN_WIDTH/2);
         setY(SettingsManager.MIDPLAYER_SIZE);
         skin = new Animation<TextureRegion>(0.25f, atlas.findRegions(AssetsManager.ATLAS_PLAYER), Animation.PlayMode.LOOP);
-
+        this.stage=stage;
     }
     @Override
     public void draw(Batch batch, float parentAlpha) {
@@ -31,6 +39,50 @@ public class PlayerSpaceShip extends Actor {
         TextureRegion currentFrame = skin.getKeyFrame(GameManager.getSingleton().getGameTime(), true);
         batch.draw(currentFrame, this.getX(), this.getY());
     }
+    // COMENZAMOS A IMPLEMENTAR EL NUEVO CODIGO
+    //creo dos array, uno de disparos activos, y otro de los que salen de la pantalla
+
+    @Override
+    public void act(float delta) {
+
+        super.act(delta);
+       // PlayerShot bala= new PlayerShot(this);
+        timeToFire+= delta;
+        /*if(timeToFire>5){
+            stage.addActor(bala);
+            //bala.act(delta);
+            timeToFire=0;
+        }*/
+
+
+        PlayerShot bala;
+        if(disparosActivos.size!=0){
+            if(disparosActivos.get(0).getY()>SettingsManager.SCREEN_HEIGHT){
+                descartar(disparosActivos.get(0));
+                disparosActivos.removeIndex(0);
+            }
+        }
+       if(timeToFire>SettingsManager.FIRE_RATE_PLAYER){
+            if(recamara.isEmpty()){
+                bala= new PlayerShot(this);
+                stage.addActor(bala);
+                disparosActivos.add(bala);
+            }else{
+                bala= recamara.get(0);
+                recamara.removeIndex(0);
+                bala.setY(this.getY()+SettingsManager.PLAYER_SIZE);
+                bala.setX(this.getX());
+                //stage.addActor(bala);
+                disparosActivos.add(bala);
+            }
+            timeToFire=0;
+        }
+    }
+    public void descartar(PlayerShot bala){
+        recamara.add(bala);
+    }
+
+    //FIN DE LA IMPLEMENTACION
 
     public void dispose() {
         if (this!=null) {
