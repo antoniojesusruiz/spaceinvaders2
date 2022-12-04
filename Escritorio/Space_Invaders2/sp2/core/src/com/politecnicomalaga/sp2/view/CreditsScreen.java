ded889fbffca0399c6397379f0b32f38b153919d
amormun062 <117466168+amormun062@users.noreply.github.com>
2022-12-04 03:32:46 +0100

    Corrección sonido botones

--- Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/view/CreditsScreen.java
+++ Escritorio/Space_Invaders2/sp2/core/src/com/politecnicomalaga/sp2/view/CreditsScreen.java
@@ -18,6 +18,7 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.politecnicomalaga.sp2.managers.AssetsManager;
 import com.politecnicomalaga.sp2.managers.LanguageManager;
 import com.politecnicomalaga.sp2.managers.ScreensManager;
+import com.politecnicomalaga.sp2.managers.SoundsManager;
 
 public class CreditsScreen implements Screen {
 
